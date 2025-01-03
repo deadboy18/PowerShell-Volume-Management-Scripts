0b33cdff522f2e3b57fb61ca6fd05e4f4a315857
deadboy18 <53135082+deadboy18@users.noreply.github.com>
2025-01-03 17:53:05 +0800

    Add files via upload

---
+++ MuteVolume1.ps1
@@ -0,0 +1,5 @@
+# Set playback volume to 1%
+Set-AudioDevice -PlaybackVolume 1
+
+# Mute the playback device
+Set-AudioDevice -PlaybackMute $true
---
+++ SetVolume70.ps1
@@ -0,0 +1,5 @@
+# Set playback volume to 70%
+Set-AudioDevice -PlaybackVolume 70
+
+# Unmute the playback device
+Set-AudioDevice -PlaybackMute $false
